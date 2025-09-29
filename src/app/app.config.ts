a39d205cf9d498f792fcc89ba6239e32b5d5ca21
Cesar- <legende.e@hotmail.com>
2025-09-29 17:49:26 -0500

    Initial commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,31 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig, inject,
+  provideAppInitializer,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import {provideHttpClient} from '@angular/common/http';
+import {provideTranslateService, TranslateService} from '@ngx-translate/core';
+import {provideTranslateHttpLoader} from '@ngx-translate/http-loader';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes), provideClientHydration(withEventReplay()),
+    provideHttpClient(),
+    provideTranslateService({
+      loader: provideTranslateHttpLoader({prefix: './assets/i18n/', suffix: '.json'}),
+      lang: 'en',
+      fallbackLang: 'en'
+    }),
+    provideAppInitializer(() => {
+      const translate = inject(TranslateService);
+      translate.use(translate.getBrowserLang() || "en");
+    })
   ]
 };
