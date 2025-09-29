e28f36ab093a0aaa5ffec1386c13915e9cb33a01
Cesar- <legende.e@hotmail.com>
2025-09-29 18:59:11 -0500

    feat: add authentication and layout components with associated styles and templates

---
+++ src/app/iam/presentation/views/login-owner/login-owner.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-owner',
+  imports: [],
+  templateUrl: './login-owner.html',
+  styleUrl: './login-owner.css'
+})
+export class LoginOwner {
+
+}
---
+++ src/app/iam/presentation/views/register-owner/register-owner.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-owner',
+  imports: [],
+  templateUrl: './register-owner.html',
+  styleUrl: './register-owner.css'
+})
+export class RegisterOwner {
+
+}
