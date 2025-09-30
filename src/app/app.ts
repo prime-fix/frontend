e0bb8c7d6160a3dcc33cff406671f249b882e148
Cesar- <legende.e@hotmail.com>
2025-09-29 23:03:18 -0500

    feat: implement login functionality with reactive form and layout components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {PublicLayout} from '@shared/presentation/views/public-layout/public-layout';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PublicLayout],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/shared/presentation/views/public-layout/public-layout.ts
+++ src/app/shared/presentation/views/public-layout/public-layout.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import {LoginOwner} from '@iam/presentation/views/login-owner/login-owner';
 
 @Component({
   selector: 'app-public-layout',
-  imports: [],
+  imports: [LoginOwner],
   templateUrl: './public-layout.html',
   styleUrl: './public-layout.css'
 })
