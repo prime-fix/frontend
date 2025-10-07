c2952143577a0a56a219be5c9cb75db3080c1c90
Renso Julca <u202121579@upc.edu.pe>
2025-10-07 13:50:22 -0500

    fea

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,5 @@
 import { Routes } from '@angular/router';
-import {Diagnosis} from './vehicle-diagnosis/presentation/views/diagnosis/diagnosis';
-import {Status} from './vehicle-status/presentation/views/status/status';
-import {StatusVehicleForm} from './vehicle-status/presentation/views/status-vehicle-form/status-vehicle-form';
-import {StatusVehicleList} from './vehicle-status/presentation/views/status-vehicle-list/status-vehicle-list';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'vehicle-status', pathMatch: 'full' },
-  { path: 'vehicle-status', component: StatusVehicleList }, // página inicial
-  { path: 'vehicle-status/vehicle-diagnosis/:id', component: Diagnosis }, // sección de diagnóstico
+
 ];
