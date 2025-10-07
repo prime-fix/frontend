fae64e582eef0ffee31a96e88adb60a844f51917
PieroTM2005 <piero.tm15@gmail.com>
2025-10-07 13:41:43 -0500

    fix: some routes error

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,6 @@ import {Home} from '@shared/presentation/views/home/home';
 export const routes: Routes = [
   { path:'home',component: Home},
   { path: '', redirectTo: 'home', pathMatch: 'full'  },
-  { path:'visits', loadChildren: () => import('./data_collection/presentation/views/data.routes').then(m => m.dataRoutes) },
-  {path:'auto_repair', loadChildren:() => import('./data_collection/presentation/views/data.routes').then(m => m.dataRoutes) }
-
+  { path:'visits', loadChildren: () => import('./data_collection/presentation/views/data.routes').then(m => m.dataRoutes),data:{renderMode:'client'} },
+  {path:'auto_repair', loadChildren:() => import('./data_collection/presentation/views/data.routes').then(m => m.dataRoutes),data:{renderMode:'client'} }
 ];
