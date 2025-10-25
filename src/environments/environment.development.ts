e155d0cdf2aad2c8add29c380d82fd4f355f7776
Cesar- <legende.e@hotmail.com>
2025-10-25 18:10:06 -0500

    refactor: rename workshop catalog store to catalog store and update environment configurations

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,8 +1,43 @@
 export const environment = {
-  production: true,
-  platformProviderApiBaseUrl: 'http://localhost:3000/api/v1',
-  platformProviderLocationsEndpointPath: '/locations',
-  platformProviderAutoRepairsEndpointPath: '/autoRepairs',
-  platformProviderTechniciansEndpointPath: '/technicians',
-  logoProviderApiBaseUrl: 'https://logo.clearbit.com/'
+  production: false,
+  usePathParams: false,
+  // Provider API configuration
+  primeFixProviderApiBaseUrl: "http://localhost:3000/api/v1",
+
+  // Endpoints Paths
+  primeFixProviderAutoRepairsEndpointPath: "/auto_repairs",
+  primeFixProviderTechniciansEndpointPath: "/technicians",
+  primeFixProviderTechnicianSchedulesEndpointPath: "/technician_schedules",
+  primeFixProviderLocationsEndpointPath: "/locations",
+  primeFixProviderMembershipsEndpointPath: "/memberships",
+  primeFixProviderNotificationsEndpointPath: "/notifications",
+  primeFixProviderPaymentsEndpointPath: "/payments",
+  primeFixProviderRegisteredVehiclesEndpointPath: "/vehicles",
+  primeFixProviderRolesEndpointPath: "/roles",
+  primeFixProviderServicesEndpointPath: "/services",
+  primeFixProviderUserAccountsEndpointPath: "/user_accounts",
+  primeFixProviderUsersEndpointPath: "/users",
+  primeFixVisitsEndpointPath: "/visits",
+  primeFixExpectedVisitsEndpointPath: "/expected_visits",
+  primeFixDiagnosticsEndpointPath: "/diagnostics",
+
+  // API Key
+  primeFixProviderApiKey: 'API_KEY',
+
+  // Query Param Keys
+  autoRepairIdQueryParamKey: "id_auto_repair",
+  technicianIdQueryParamKey: "id_technician",
+  technicianScheduleIdQueryParamKey: "id_schedule",
+  locationIdQueryParamKey: "id_location",
+  membershipIdQueryParamKey: "id_membership",
+  notificationIdQueryParamKey: "id_notification",
+  paymentIdQueryParamKey: "id_payment",
+  registeredVehicleIdQueryParamKey: "id_vehicle",
+  roleIdQueryParamKey: "id_role",
+  serviceIdQueryParamKey: "id_service",
+  userAccountIdQueryParamKey: "id_user_account",
+  userIdQueryParamKey: "id_user",
+  visitIdQueryParamKey: "id_visit",
+  expectedVisitIdQueryParamKey: "id_expected",
+  diagnosticIdQueryParamKey: "id_diagnostic",
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,43 @@
 export const environment = {
-  production: false,
-  platformProviderApiBaseUrl: 'http://localhost:3000/api/v1',
-  platformProviderLocationsEndpointPath: '/locations',
-  platformProviderAutoRepairsEndpointPath: '/autoRepairs',
-  platformProviderTechniciansEndpointPath: '/technicians',
-  logoProviderApiBaseUrl: 'https://logo.clearbit.com/'
+  production: true,
+  usePathParams: true,
+  // Provider API configuration
+  primeFixProviderApiBaseUrl: "https://klrtzcjehbkfckohrvvu.supabase.co/rest/v1",
+
+  // Endpoints Paths
+  primeFixProviderAutoRepairsEndpointPath: "/auto_repairs",
+  primeFixProviderTechniciansEndpointPath: "/technicians",
+  primeFixProviderTechnicianSchedulesEndpointPath: "/technician_schedules",
+  primeFixProviderLocationsEndpointPath: "/locations",
+  primeFixProviderMembershipsEndpointPath: "/memberships",
+  primeFixProviderNotificationsEndpointPath: "/notifications",
+  primeFixProviderPaymentsEndpointPath: "/payments",
+  primeFixProviderRegisteredVehiclesEndpointPath: "/registered_vehicles",
+  primeFixProviderRolesEndpointPath: "/roles",
+  primeFixProviderServicesEndpointPath: "/services",
+  primeFixProviderUserAccountsEndpointPath: "/user_accounts",
+  primeFixProviderUsersEndpointPath: "/users",
+  primeFixVisitsEndpointPath: "/visits",
+  primeFixExpectedVisitsEndpointPath: "/expected_visits",
+  primeFixDiagnosticsEndpointPath: "/diagnostics",
+
+  // API Key
+  primeFixProviderApiKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtscnR6Y2plaGJrZmNrb2hydnZ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkxMDc4NjQsImV4cCI6MjA3NDY4Mzg2NH0.abo91QGTMisENqS_a-8QWV0WP3VZSkkOkRUitRv8iBw',
+
+  // Query Param Keys
+  autoRepairIdQueryParamKey: "id_auto_repair",
+  technicianIdQueryParamKey: "id_technician",
+  technicianScheduleIdQueryParamKey: "id_schedule",
+  locationIdQueryParamKey: "id_location",
+  membershipIdQueryParamKey: "id_membership",
+  notificationIdQueryParamKey: "id_notification",
+  paymentIdQueryParamKey: "id_payment",
+  registeredVehicleIdQueryParamKey: "id_vehicle",
+  roleIdQueryParamKey: "id_role",
+  serviceIdQueryParamKey: "id_service",
+  userAccountIdQueryParamKey: "id_user_account",
+  userIdQueryParamKey: "id_user",
+  visitIdQueryParamKey: "id_visit",
+  expectedVisitIdQueryParamKey: "id_expected",
+  diagnosticIdQueryParamKey: "id_diagnostic",
 };
