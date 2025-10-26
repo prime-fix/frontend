91837dafab9dd0e1e41402c797ee3eb565b8e83c
Cesar- <legende.e@hotmail.com>
2025-10-26 13:23:32 -0500

    feat: enhance TrackingApi and TrackingStore to manage Vehicles with CRUD operations

--- src/app/maintenance-tracking/infrastructure/tracking-api.ts
+++ src/app/maintenance-tracking/infrastructure/tracking-api.ts
@@ -4,9 +4,11 @@ import {Observable} from 'rxjs';
 import {Notification} from '@tracking/domain/model/notification.entity';
 import {NotificationApiEndpoint} from '@tracking/infrastructure/notification-api-endpoint';
 import {Injectable} from '@angular/core';
+import {VehicleApiEndpoint} from '@tracking/infrastructure/vehicle-api-endpoint';
+import {Vehicle} from '@tracking/domain/model/vehicle.entity';
 
 /**
- * API for managing tracking-related operations.
+ * API service for managing tracking-related operations such as notifications and vehicles.
  */
 @Injectable({
   providedIn: 'root'
@@ -17,10 +19,20 @@ export class TrackingApi extends BaseApi {
    * @private - Indicates that this property is private and should not be accessed directly from outside the class.
    */
   private readonly notificationsEndpoint: NotificationApiEndpoint;
+  /**
+   * The VehicleApiEndpoint instance for managing vehicles.
+   * @private
+   */
+  private readonly vehicleEndpoint:     VehicleApiEndpoint;
 
+  /**
+   * Constructs a new instance of the TrackingApi.
+   * @param http - The HttpClient used for making HTTP requests.
+   */
   constructor(http: HttpClient) {
     super();
     this.notificationsEndpoint = new NotificationApiEndpoint(http);
+    this.vehicleEndpoint = new VehicleApiEndpoint(http);
   }
 
   /**
@@ -65,5 +77,49 @@ export class TrackingApi extends BaseApi {
    */
   deleteNotification(id: string): Observable<void> {
     return this.notificationsEndpoint.delete(id);
+  }
+
+  /**
+   * Gets all Vehicles.
+   * @returns An Observable of an array of Vehicles.
+   */
+  getVehicles(): Observable<Vehicle[]> {
+    return this.vehicleEndpoint.getAll();
+  }
+
+  /**
+   * Gets a Vehicle by its ID.
+   * @param id - The ID of the Vehicle.
+   * @returns An Observable of the Vehicle.
+   */
+  getVehicleById(id: number): Observable<Vehicle> {
+    return this.vehicleEndpoint.getById(id);
+  }
+
+  /**
+   * Creates a new Vehicle.
+   * @param vehicle - The Vehicle to create.
+   * @returns An Observable of the created Vehicle.
+   */
+  createVehicle(vehicle: Vehicle): Observable<Vehicle> {
+    return this.vehicleEndpoint.create(vehicle);
+  }
+
+  /**
+   * Updates an existing Vehicle.
+   * @param vehicle - The Vehicle to update.
+   * @returns An Observable of the updated Vehicle.
+   */
+  updateVehicle(vehicle: Vehicle): Observable<Vehicle> {
+    return this.vehicleEndpoint.update(vehicle, vehicle.id);
+  }
+
+  /**
+   * Deletes a Vehicle by its ID.
+   * @param id - The ID of the Vehicle to delete.
+   * @returns An Observable of void.
+   */
+  deleteVehicle(id: number | string): Observable<void> {
+    return this.vehicleEndpoint.delete(id);
   }
 }
